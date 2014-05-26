33c846e08abf43d93b91c36c445d23bd3ab0e839
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2014-05-26 15:23:30 +0900

    Initial Commit

--- AW13/ArraySample/ArraySample/ViewController.m
+++ AW13/ArraySample/ArraySample/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
+
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *labels;
 
 @end
 
@@ -18,6 +23,78 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    /*
+    NSArray *array = [NSArray arrayWithObjects:self.label1, self.label2, self.label3, nil];
+    int n = [array count];
+    for (int i = 0; i < n; i++) {
+        UILabel *label = [array objectAtIndex:i];
+        [label setBackgroundColor:[UIColor redColor]];
+    }
+     */
+
+    /*
+    NSArray *array = @[self.label1, self.label2, self.label3]; ///////
+    int n = [array count];
+    for (int i = 0; i < n; i++) {
+        UILabel *label = [array objectAtIndex:i];
+        [label setBackgroundColor:[UIColor redColor]];
+    }
+     */
+
+    /*
+    NSArray *array = @[self.label1, self.label2, self.label3];
+    int n = array.count; /////
+    for (int i = 0; i < n; i++) {
+        UILabel *label = [array objectAtIndex:i];
+        [label setBackgroundColor:[UIColor redColor]];
+    }
+     */
+
+    /*
+    NSArray *array = @[self.label1, self.label2, self.label3];
+    int n = array.count;
+    for (int i = 0; i < n; i++) {
+        UILabel *label = [array objectAtIndex:i];
+        [label setBackgroundColor:UIColor.redColor]; /////
+    }
+     */
+
+    /*
+    NSArray *array = @[self.label1, self.label2, self.label3];
+    int n = array.count;
+    for (int i = 0; i < n; i++) {
+        UILabel *label = [array objectAtIndex:i];
+        label.backgroundColor = UIColor.redColor; /////
+    }
+     */
+
+    /*
+    NSArray *array = @[self.label1, self.label2, self.label3];
+    int n = array.count;
+    for (int i = 0; i < n; i++) {
+        UILabel *label = array[i]; /////
+        label.backgroundColor = UIColor.redColor;
+    }
+     */
+
+    /*
+    NSArray *array = @[self.label1, self.label2, self.label3];
+    for (UILabel *label in array) { ////
+        label.backgroundColor = UIColor.redColor;
+    }
+     */
+
+    for (UILabel *label in self.labels) { ////
+        label.backgroundColor = UIColor.redColor;
+        //label.backgroundColor = [UIColor redColor];
+    }
+
+    /*
+    [self.labels enumerateObjectsUsingBlock:^(UILabel *label, NSUInteger idx, BOOL *stop) {
+        label.backgroundColor = UIColor.redColor;
+    }];
+     */
 }
 
 - (void)didReceiveMemoryWarning
