6af869aeff7da142049cf25c872d6f2b7a668b6d
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2014-06-30 15:59:06 +0900

    Initial Commit

--- AW13/Dice2/Dice2/ViewController.m
+++ AW13/Dice2/Dice2/ViewController.m
@@ -9,21 +9,67 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *rollButton;
 
 @end
 
 @implementation ViewController
+{
+    NSArray *_imageNames;
+    int _eye;
+    int _intervalIndex;
+    NSArray *_intervals;
+}
+
+- (void)updateEye
+{
+    int n = arc4random() % _imageNames.count;
+    self.imageView.image = [UIImage imageNamed:_imageNames[n]];
+}
+
+- (void)roll
+{
+    [self updateEye];
+    if (_intervalIndex < _intervals.count) {
+        NSNumber *interval = _intervals[_intervalIndex];
+        [NSTimer scheduledTimerWithTimeInterval:[interval floatValue]
+                                         target:self
+                                       selector:@selector(roll)
+                                       userInfo:nil
+                                        repeats:NO];
+        _intervalIndex++;
+    } else {
+        self.rollButton.enabled = YES;
+    }
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _imageNames = @[
+                    @"one", @"two", @"three",
+                    @"four", @"five", @"six",
+                    ];
+    _intervals = @[
+                   @0.1, @0.2, @0.3, @0.4, @0.5,
+                   @0.6, @0.7, @0.8, @0.9, @1.0,
+                   ];
+    [self updateEye];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)roll:(id)sender {
+    _intervalIndex = 0;
+    self.rollButton.enabled = NO;
+    [self roll];
 }
 
 @end
---
+++ AW13/Timer/Timer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Timer
+//
+//  Created by Naoto Yoshioka on 2014/06/30.
+//  Copyright (c) 2014年 Naoto Yoshioka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
