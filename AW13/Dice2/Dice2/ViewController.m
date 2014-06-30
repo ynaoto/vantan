c537420008953d9eb4b5985ec68aa591c413b743
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2014-06-30 15:18:33 +0900

    Initial Commit

--- AW13/Dice2/Dice2/ViewController.m
+++ AW13/Dice2/Dice2/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  Dice1
+//  Dice2
 //
 //  Created by Naoto Yoshioka on 2014/06/30.
 //  Copyright (c) 2014年 Naoto Yoshioka. All rights reserved.
@@ -9,42 +9,21 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
-{
-    NSArray *_imageNames;
-    int _eye;
-}
-
-- (void)updateEye
-{
-    int n = arc4random() % _imageNames.count;
-    self.imageView.image = [UIImage imageNamed:_imageNames[n]];
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    _imageNames = @[
-                @"one", @"two", @"three",
-                @"four", @"five", @"six",
-                ];
-    [self updateEye];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)roll:(id)sender {
-    [self updateEye];
 }
 
 @end
---
+++ AW13/Dice2/Dice2Tests/Dice2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Dice2Tests.m
+//  Dice2Tests
+//
+//  Created by Naoto Yoshioka on 2014/06/30.
+//  Copyright (c) 2014年 Naoto Yoshioka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dice2Tests : XCTestCase
+
+@end
+
+@implementation Dice2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
