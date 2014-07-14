bcae0418ffdb65c4801c87ffc501cde74d54d348
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2014-07-15 07:27:02 +0900

    PickerView sample

--- AW13/PickerViewSample/PickerViewSample/ViewController.m
+++ AW13/PickerViewSample/PickerViewSample/ViewController.m
@@ -8,16 +8,45 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
 
 @end
 
 @implementation ViewController
+{
+    NSArray *_dataArray;
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return _dataArray.count;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    NSArray *a = _dataArray[component];
+    return a.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    NSArray *a = _dataArray[component];
+    return a[row];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//    _dataArray = @[
+//                   @[ @"A", @"B", @"C" ],
+//                   @[ @"1", @"2", @"3", @"4", @"5" ],
+//                   @[ @"あ", @"い", @"う", @"え", @"お"],
+//                   ];
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    NSURL *url = [mainBundle URLForResource:@"data" withExtension:@"plist"];
+    _dataArray = [NSArray arrayWithContentsOfURL:url];
 }
 
 - (void)didReceiveMemoryWarning
